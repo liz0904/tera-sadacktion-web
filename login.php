39fe1264dd5eb09ce492a1b70acd1f270dac3233
liz0904 <liz09045@naver.com>
2022-08-31 14:33:22 +0000

    login 기능 수정

--- login.php
+++ login.php
@@ -41,18 +41,17 @@
                                     <div class="text-center">
                                         <h1 class="h4 text-gray-900 mb-4">테라싸닥션 관리자 로그인</h1>
                                     </div>
-                                    <form class="user">
+                                    <form method="post" action="login_action.php" class=user >
                                         <div class="form-group">
                                             <input type="text"	class="form-control form-control-user"
-                                                id="username"	placeholder="username">
+                                                name="username"	placeholder="username">
                                         </div>
                                         <div class="form-group">
                                             <input type="password" class="form-control form-control-user"
-                                                id="password" placeholder="Password">
+                                                name="password" placeholder="Password">
                                         </div>
                                        
-                                        <a href="login_action.php" class="btn btn-primary btn-user btn-block">
-                                            Login
+                                        <input type="submit" value="login" class="btn btn-primary btn-user btn-block">
                                         </a>
                                     </form>
                                     <hr>
